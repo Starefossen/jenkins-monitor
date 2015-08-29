6e074e40c5418964b5b937c9e4578d649b611702
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2015-08-29 10:52:41 +0200

    Add date time to all console log output

--- index.js
+++ index.js
@@ -9,33 +9,33 @@ const gitter  = require('./lib/gitter');
 const sendgrid = require('./lib/sendgrid');
 
 const pkg = require('./package.json');
-console.log(`Staring ${pkg.name} v${pkg.version}`);
+console.log(new Date(), `Staring ${pkg.name} v${pkg.version}`);
 
 schedule.scheduleJob(process.env.CRON_INTERVAL, function() {
-  console.log('Running Cron Job...');
+  console.log(new Date(), 'Running Cron Job...');
 
-  console.log('Fetching Jenkins nodes...');
+  console.log(new Date(), 'Fetching Jenkins nodes...');
   jenkins.getComputers(function(err, nodes) {
     if (err) { throw err; }
-    console.log(`Found ${nodes.length} Jenkins nodes.`);
+    console.log(new Date(), `Found ${nodes.length} Jenkins nodes.`);
 
-    console.log('Checking changed Jenkins nodes...');
+    console.log(new Date(), 'Checking changed Jenkins nodes...');
     redis.jenkinsChanged(nodes, function(err, changed) {
       if (err) { throw err; }
 
-      console.log(`${changed.length} node(s) changed.`);
+      console.log(new Date(), `${changed.length} node(s) changed.`);
 
       if (changed.length > 0) {
-        console.log('Posting to Gitter...');
+        console.log(new Date(), 'Posting to Gitter...');
         gitter.post(changed, function(err) {
           if (err) { throw err; }
-          console.log('Gitter: Ok!');
+          console.log(new Date(), 'Gitter: Ok!');
         });
 
-        console.log('Notifying via Sendgrid...');
+        console.log(new Date(), 'Notifying via Sendgrid...');
         sendgrid.notify(changed, function(err) {
           if (err) { throw err; }
-          console.log('Sendgrid: Ok!');
+          console.log(new Date(), 'Sendgrid: Ok!');
         });
       }
     });
