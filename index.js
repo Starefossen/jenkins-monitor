f1bebcff15d14c6e22ba84fd2692d302b9b601af
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-01-26 20:22:06 +0100

    fix(eslint): prefer arrow callbacks

--- index.js
+++ index.js
@@ -14,35 +14,35 @@ const pkg = require('./package.json');
 console.log(new Date(), `Staring ${pkg.name} v${pkg.version}`);
 console.log(new Date(), process.env.SENDGRID_RECIPIENTS.split(','));
 
-schedule.scheduleJob(process.env.CRON_INTERVAL, function scheduleJobCb() {
+schedule.scheduleJob(process.env.CRON_INTERVAL, () => {
   console.log(new Date(), 'Running Cron Job...');
 
   console.log(new Date(), 'Fetching Jenkins nodes...');
-  jenkins.getComputers(function getComputersCb(jenkinsErr, nodes) {
+  jenkins.getComputers((jenkinsErr, nodes) => {
     if (jenkinsErr) { throw jenkinsErr; }
     console.log(new Date(), `Found ${nodes.length} Jenkins nodes.`);
 
     console.log(new Date(), 'Checking changed Jenkins nodes...');
-    redis.jenkinsChanged(nodes, function jenkinsChangedCb(redisErr, changed) {
+    redis.jenkinsChanged(nodes, (redisErr, changed) => {
       if (redisErr) { throw redisErr; }
 
       console.log(new Date(), `${changed.length} node(s) changed.`);
 
       if (changed.length > 0) {
         console.log(new Date(), 'Posting to Gitter...');
-        gitter.notify(changed, function gitterPostCb(err) {
+        gitter.notify(changed, (err) => {
           if (err) { throw err; }
           console.log(new Date(), 'Gitter: Ok!');
         });
 
         console.log(new Date(), 'Notifying via Sendgrid...');
-        sendgrid.notify(changed, function sendgridNotifyCb(err) {
+        sendgrid.notify(changed, (err) => {
           if (err) { throw err; }
           console.log(new Date(), 'Sendgrid: Ok!');
         });
 
         console.log(new Date(), 'Notifying via IRC...');
-        irc.notify(changed, function ircPostCb(err) {
+        irc.notify(changed, (err) => {
           if (err) { throw err; }
           console.log(new Date(), 'IRC: Ok!');
         });
