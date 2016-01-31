e491791bd37d47f59927382af6fec1f109cc1611
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-01-31 23:19:52 +0100

    feat(redis): add downtime threshold before reporting nodes as offline
    BREAKING CHANGE: Nodes are no longer just stored as `offline` = `0` or `1`. This
    is now an incrementing field for the number of times a node has been seen
    offline in a row. Nodes are only reported offline once they have been offline
    for `OFFLINE_THRES` times and will not be reported as online unless this
    threashold was reached.

--- test/redis.js
+++ test/redis.js
@@ -4,24 +4,7 @@
 const assert = require('assert');
 const redis = require('../lib/redis');
 
-const jsonist = require('jsonist');
-const jenkins = require('../lib/jenkins');
-const _get = jsonist.get;
-
-let nodes;
-
 describe('redis', function () {
-  before(function () {
-    jsonist.get = function (url, opts, cb) {
-      const computers = require('./assets/computers.json');
-      return cb(null, JSON.parse(JSON.stringify(computers)));
-    };
-  });
-
-  after(function () {
-    jsonist.get = _get;
-  });
-
   beforeEach(function (done) {
     this.timeout(10000);
     redis.flushdb(done);
@@ -29,10 +12,13 @@ describe('redis', function () {
 
   beforeEach(function (done) {
     this.timeout(10000);
-    jenkins.getComputers(function (err, computers) {
-      nodes = computers;
-      done(err);
-    });
+
+    redis.hset('node:foo', 'offline', 0);
+    redis.hset('node:bar', 'offline', 1);
+    redis.hset('node:baz', 'offline', 2);
+    redis.hset('node:bix', 'offline', 3);
+    redis.hset('node:bez', 'offline', 4);
+    redis.hset('node:pax', 'offline', 5, done);
   });
 
   describe('module.exports', function () {
@@ -42,41 +28,70 @@ describe('redis', function () {
   });
 
   describe('jenkinsChanged()', function () {
-    it('returns offline nodes', function (done) {
+    it('returns empty array for no offline nodes', function (done) {
+      const nodes = [{ name: 'foo', offline: 0 }];
+
       redis.jenkinsChanged(nodes, function (err, n) {
         assert.ifError(err);
-        assert.equal(n.length, 3);
-        assert.equal(n[0].name, 'iojs-linaro-armv8-ubuntu1404');
-        assert.equal(n[1].name, 'iojs-nodesource-raspbian-wheezy-pi1p-3');
-        assert.equal(n[2].name, 'iojs-voxer-osx1010-release-pkg-2');
+        assert.equal(n.length, 0);
+        assert.deepEqual(n, {});
         done();
       });
     });
 
-    it('returns empty array for no changed nodes', function (done) {
-      redis.jenkinsChanged(nodes, function (redisErr1) {
-        assert.ifError(redisErr1);
-        nodes = JSON.parse(JSON.stringify(require('./assets/computers')));
-        redis.jenkinsChanged(nodes, function (redisErr2, n) {
-          assert.ifError(redisErr2);
-          assert.deepEqual(n, []);
-          done();
+    it('returns offline nodes after 3 consecutive times offline', function (done) {
+      const nodes = [
+        { name: 'foo', offline: 1 },
+        { name: 'bar', offline: 1 },
+        { name: 'baz', offline: 1 },
+        { name: 'bix', offline: 1 },
+        { name: 'bez', offline: 1 },
+        { name: 'pax', offline: 1 },
+      ];
+
+      redis.jenkinsChanged(nodes, function (err1, n1) {
+        assert.ifError(err1);
+        assert.equal(n1.length, 1);
+        assert.equal(n1[0].name, 'baz');
+
+        redis.jenkinsChanged(nodes, function (err2, n2) {
+          assert.ifError(err2);
+          assert.equal(n2.length, 1);
+          assert.equal(n2[0].name, 'bar');
+
+          redis.jenkinsChanged(nodes, function (err3, n3) {
+            assert.ifError(err3);
+            assert.equal(n3.length, 1);
+            assert.equal(n3[0].name, 'foo');
+
+            done();
+          });
         });
       });
     });
 
-    it('returns only changed nodes', function (done) {
-      redis.jenkinsChanged(nodes, function (redisErr1) {
-        assert.ifError(redisErr1);
+    it('returns online nodes after 3+ consecutive times offline', function (done) {
+      const nodes = [
+        { name: 'foo', offline: 0 },
+        { name: 'bar', offline: 0 },
+        { name: 'baz', offline: 0 },
+        { name: 'bix', offline: 0 },
+        { name: 'bez', offline: 0 },
+        { name: 'pax', offline: 0 },
+      ];
 
-        nodes[5].offline = 1;
-        nodes[6].offline = 1;
+      redis.jenkinsChanged(nodes, function (err1, n1) {
+        assert.ifError(err1);
+        assert.equal(n1.length, 3);
+        assert.equal(n1[0].name, 'bix');
+        assert.equal(n1[1].name, 'bez');
+        assert.equal(n1[2].name, 'pax');
 
-        redis.jenkinsChanged(nodes, function (redisErr2, n) {
-          assert.ifError(redisErr2);
-          assert.equal(n.length, 2);
-          assert.equal(n[0].name, 'iojs-digitalocean-centos5-release-32-1');
-          assert.equal(n[1].name, 'iojs-digitalocean-centos5-release-64-1');
+        redis.jenkinsChanged(nodes, function (err2, n2) {
+          assert.ifError(err2);
+          assert.equal(n2.length, 0);
+          assert.deepEqual(n2, {});
+
           done();
         });
       });
